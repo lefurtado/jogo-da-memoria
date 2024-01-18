cdcded16961ccd7e3ce16a12052e1ce486608b8e
Leandro Furtado <le_furtado@outlook.com.br>
2024-01-17 23:34:25 -0300

    add: modificações nos caminhos das imagens

--- src/data/cards.js
+++ src/data/cards.js
@@ -1,38 +1,38 @@
 export const cards = [
   {
     id: '1',
-    back: 'assets/images/01.png'
+    back: '/src/assets/images/01.png'
   },
   {
     id: '2',
-    back: 'assets/images/02.png'
+    back: '/src/assets/images/02.png'
   },
   {
     id: '3',
-    back: 'assets/images/03.png'
+    back: '/src/assets/images/03.png'
   },
   {
     id: '4',
-    back: 'assets/images/04.png'
+    back: '/src/assets/images/04.png'
   },
   {
     id: '5',
-    back: 'assets/images/05.png'
+    back: '/src/assets/images/05.png'
   },
   {
     id: '6',
-    back: 'assets/images/06.png'
+    back: '/src/assets/images/06.png'
   },
   {
     id: '7',
-    back: 'assets/images/07.png'
+    back: '/src/assets/images/07.png'
   },
   {
     id: '8',
-    back: 'assets/images/08.png'
+    back: '/src/assets/images/08.png'
   },
   {
     id: '9',
-    back: 'assets/images/09.png'
+    back: '/src/assets/images/09.png'
   },
 ]
